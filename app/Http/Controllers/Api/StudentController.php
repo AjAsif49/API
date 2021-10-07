b0861e9d8c4bd2f2586a92c6668ee21fc6c61622
AjAsif49 <54905485+AjAsif49@users.noreply.github.com>
2021-10-07 19:53:46 +0600

    CRUD

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use DB;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = DB::table('students')->get();
+        return response()->json($students);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = array();
+        $data['class_id'] = $request->class_id;
+        $data['section_id'] = $request->section_id;
+        $data['name'] = $request->name;
+        $data['phone'] = $request->phone;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+        $data['photo'] = $request->photo;
+        $data['address'] = $request->address;
+        $data['gender'] = $request->gender;
+        DB::table('students')->insert($data);
+        return response('Inserted Successfully');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $student = DB::table('students')->where('id', $id)->first();
+        return response()->json($student);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = array();
+        $data['class_id'] = $request->class_id;
+        $data['section_id'] = $request->section_id;
+        $data['name'] = $request->name;
+        $data['phone'] = $request->phone;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+        $data['photo'] = $request->photo;
+        $data['address'] = $request->address;
+        $data['gender'] = $request->gender;
+        DB::table('students')->where('id',$id)->update($data);
+        return response('Updated Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $img = DB::table('students')->where('id', $id)->first(); //get the first data
+        $image_path = $img->photo; //get the image
+        unlink($image_path); //delete the image from folder
+        DB::table('students')->where('id', $id)->delete(); //delete all data
+        return response('Deleted');
+    }
+}
